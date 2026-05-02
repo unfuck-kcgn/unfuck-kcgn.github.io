f19d4a63195966500a167ffce774bb6e361ed46e
gallantron <25206391+gallantron@users.noreply.github.com>
2026-05-02 15:45:35 +0200

    fix for weirdness

--- index.js
+++ index.js
@@ -11,30 +11,31 @@ const downloadFile = ((mimeType, filename, fileContent) => {
 const parseAndDownload = ((filename, ktsFile, kcgnAssigned) => {
     const assignedTables = kcgnAssigned.split('\n').map((s) => {
         const t = s.split(',');
-        return [parseInt(t[1]),parseInt(t[2])];
-    }).sort((a,b) => a[0]-b[0]);
+        return [parseInt(t[0]),parseInt(t[1])];
+    }).sort((a,b) => b[1]-a[1]);
     const parsedKts = (new DOMParser()).parseFromString(ktsFile, 'text/xml');
-    //const foundTables = new Set();
+    const foundTables = new Set();
     parsedKts.querySelectorAll('Matches > TournMatch').forEach((match) => {
         const players = [...(match.querySelectorAll('Player'))].map((e) => parseInt(e.textContent));
         const round = match.querySelector('Round').textContent;
         let tableElm = match.querySelector('Table');
         const oldTable = parseInt(tableElm.textContent);
         let newTable = oldTable;
-        for (const [fixedSeat, fixedPlayer] of assignedTables) {
-            if (players.some((it) => (it === fixedPlayer))) {
+        for (const [fixedSeatPos, fixedSeat] of assignedTables) {
+            if (oldTable === fixedSeatPos) {
                 newTable = fixedSeat;
                 break;
             } else {
-                if (newTable <= fixedSeat) newTable += 1;
+                if (newTable <= fixedSeat) newTable -= 1;
             }
         }
         console.log('Round',round,'Table',oldTable,'now becomes',newTable);
-        /*if (foundTables.has(newTable)) {
+        const ident = JSON.stringify({round,newTable});
+        if (foundTables.has(ident)) {
             window.alert('Duplicate reassigned table. Something has gone wrong.');
             return;
         }
-        foundTables.add(newTable);*/
+        foundTables.add(ident);
         tableElm.textContent = (''+newTable);
     });
     const serializedKts = (new XMLSerializer()).serializeToString(parsedKts);
